9abc79ba49ff8e38e9fb29f4eb457eca132aebb5
mngai99 <mngai99@gmail.com>
2022-09-15 14:36:38 -0400

    Updated words in header

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export default class Header extends React.Component {
                 <div className={styles.title}>
                     <img className={styles.mainLogo} src="/images/logo.png" alt="App Logo" width={75} height={"auto"} />
                     <h1 className={styles.header}>GeneRanger</h1>
-                    <div className={styles.text}>This web app takes the input of a human gene and displays its expression across human cells and tissues utilizing a variety of processed datasets from healthy tissues. If the gene is not contained in one of the datasets, a plot will not be produced for that resource.</div>
+                    <div className={styles.text}>GeneRanger takes the input of a human gene and displays its expression across human cells and tissues utilizing a variety of processed datasets from healthy tissues. If the gene is not contained in one of the datasets, a plot will not be produced for that resource.</div>
                 </div>
             
             </>
