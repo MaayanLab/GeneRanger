4f96a5de067e21345467fb88eed31779eabaf541
mngai99 <mngai99@gmail.com>
2022-10-13 11:23:50 -0400

    Update create_CCLE_proteomics.py

--- database scripts/create_CCLE_proteomics.py
+++ database scripts/create_CCLE_proteomics.py
@@ -6,12 +6,13 @@ df = pd.read_csv('protein_quant_current_normalized.csv')
 
 print("Loaded protein_quant_current_normalized.csv")
 
-gene_df = df[["Gene_Symbol"]]
-values = df.iloc[:, 48:]
+# Removing unnecessary columns.  These two dataframes will be recombined later
+gene_df = df[["Gene_Symbol"]] # Gene names
+values = df.iloc[:, 48:] # Tissue names and values
 
 tissues = list(values.columns.values)
-tissues.pop(0) # Removes "gene_symbols"
 
+# Removing extra info from tissue names
 for i in range(len(tissues)):
     tissue = tissues[i]
     tissues[i] = tissue[tissue.find("_")+1:tissue.rfind("_")]
@@ -21,10 +22,12 @@ for i in range(len(tissues)):
 
 values.sort_index(axis=1, inplace=True)
 
+# Combining gene df and values df back together
 df = pd.concat([gene_df, values], axis=1)
 
 print("Sorted and processed tissue names")
 
+# Extracted data that will be inserted into the final dataframe
 gene_names = [None] * (len(df) * (len(df.columns) - 1))
 tissues = [None] * (len(df) * (len(df.columns) - 1))
 values = [None] * (len(df) * (len(df.columns) - 1))
@@ -53,6 +56,6 @@ print('Inserted data into dataframe')
 
 final_df.to_csv("CCLE_proteomics.tsv", sep = "\t")
 
-print('Created tsv file')
+print('Created .tsv file')
 
 print('Done!')
