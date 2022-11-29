18d2a209a5788389058c790af49540fde69a4230
Daniel Clarke <u8sand@gmail.com>
2022-11-29 18:17:06 -0500

    Fixes "the labels used to be "tissue, [newline] cell" but now they're "cell - tissue""

--- ETL/00_preprocess_hpa.py
+++ ETL/00_preprocess_hpa.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('input/hpa.tsv', sep='\t').dropna()
 df['label'] = 'value'
-df['description'] = df.apply(lambda r: f"{r['Cell.type']} - {r['Tissue']}", axis=1)
+df['description'] = df.apply(lambda r: f"{r['Cell.type']},\n{r['Tissue']}", axis=1)
 df = df.groupby(['Gene.name', 'label', 'description'])['Level'].first().unstack()
 df.to_csv('preprocessed/hpa.tsv', sep='\t')
