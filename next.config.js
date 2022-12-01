3d69a9d4419fdf9f824a6418b17df0eb85618ca7
Daniel J. B. Clarke <u8sand@gmail.com>
2022-12-01 16:19:24 -0500

    Use next.config.js redirect instead of staticProp redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/gene/A2M?currDatabase=0',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
