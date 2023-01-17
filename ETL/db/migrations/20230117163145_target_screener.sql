8f3083504299862169cc15806fcec24d6541989b
Daniel Clarke <u8sand@gmail.com>
2023-01-17 14:02:31 -0500

    Compute t-statistic across all tissues

--- ETL/db/migrations/20230117163145_target_screener.sql
+++ ETL/db/migrations/20230117163145_target_screener.sql
@@ -2,27 +2,6 @@
 
 create extension if not exists "plpython3u";
 
--- { a: { b: c, ... }, ... } => { b: { a: c, ... } }
-create function jsonb_transpose(df jsonb) returns jsonb as $$
-with
-  flat as (
-    select
-      a.key as index, b.key as column, b.value as value
-    from
-      jsonb_each(df) as a
-      cross join lateral jsonb_each(a.value) as b
-  ),
-  column_index_values as (
-    select
-      flat.column,
-      jsonb_object_agg(flat.index, flat.value) as index_values
-    from flat
-    group by flat.column
-  )
-select jsonb_object_agg(column_index_values.column, column_index_values.index_values)
-from column_index_values
-$$ language sql immutable;
-
 create type welchs_t_test_results AS (
   t double precision,
   p double precision
@@ -36,30 +15,66 @@ create function welchs_t_test(a_mean double precision, a_std double precision, a
   return (result.statistic, result.pvalue)
 $$ language plpython3u immutable;
 
+-- given stats of the form { mean: { desc1: value, desc2: value, ... }, std: {...}, count: {...} }
+-- compute an aggregated distribution by:
+--  mean(means)
+--  sqrt(sum(std**2))
+--  sum(count)
+create function aggregate_stats(stats jsonb) returns jsonb as $$
+  select jsonb_build_object(
+    'mean',
+    (
+      select to_jsonb(avg(j.value::text::double precision))
+      from jsonb_each(stats->'mean') j
+    ),
+    'std',
+    (
+      select to_jsonb(sqrt(sum(power(j.value::text::double precision,2))))
+      from jsonb_each(stats->'std') j
+    ),
+    'count',
+    (
+      select to_jsonb(sum(j.value::text::double precision))
+      from jsonb_each(stats->'count') j
+    )
+  )
+$$ language sql immutable;
+
+-- Here we store an aggregated version of data, this will be much
+--  smaller and cheaper to query against. It must be refreshed if
+--  data is added.
+create materialized view database_agg as
+select
+  data.id as id,
+  data.database as database,
+  data.gene as gene,
+  aggregate_stats(data.values) as values
+from data;
 
 create type screen_target_results AS (
   gene varchar,
-  description varchar,
   t double precision,
   p double precision
 );
 
 -- Usage:
 -- ```sql
--- select gene, description, t, p
+-- select gene, t, p
 -- from screen_targets(
---   '{"n": 5, "genes": {"STAT3": {"mean": 120, "std": 16}, "ACE2": {"mean": 150, "std": 20}}}'::jsonb,
+--   '{"n": 5, "genes": {"STAT3": {"mean": 5400, "std": 16}, "ACE2": {"mean": 150, "std": 20}}}'::jsonb,
 --   (
 --     select database.id
 --     from database
 --     where database.dbname = 'ARCHS4'
 --     limit 1
 --   )
--- );
+-- )
+-- where p < 0.05
+-- order by t desc;
 -- ```
 -- In JS:
 -- ```sql
--- select gene, description, t, p
+-- select gene, t, p
 -- from screen_targets(
 --   ${input_data}::jsonb,
 --   (
@@ -68,7 +83,9 @@ create type screen_target_results AS (
 --     where database.dbname = ${input_database}
 --     limit 1
 --   )
--- );
+-- )
+-- where p < 0.05
+-- order by t desc;
 -- ```
 
 create function screen_targets(input_data jsonb, background uuid) returns setof screen_target_results as $$
@@ -82,19 +99,16 @@ create function screen_targets(input_data jsonb, background uuid) returns setof 
   input_background as (
     select
       input_data_each.gene,
-      j.key as description,
       input_data_each.values as input_data,
-      j.value as background_data
+      database_agg.values as background_data
     from input_data_each
     inner join gene on gene.gene = input_data_each.gene
-    inner join data on data.gene = gene.id
-    cross join lateral jsonb_each(jsonb_transpose(data.values)) j
-    where data.database = background
+    inner join database_agg on database_agg.gene = gene.id
+    where database_agg.database = background
   ),
   stats as (
     select
       gene,
-      description,
       r.t as t,
       (1-r.p) as p
     from input_background
@@ -116,6 +130,7 @@ $$ language sql immutable;
 -- migrate:down
 drop function screen_targets(jsonb, uuid) cascade;
 drop type screen_target_results cascade;
+drop materialized view database_agg;
+drop function aggregate_stats(jsonb);
 drop function welchs_t_test(double precision, double precision, double precision, double precision, double precision, double precision, boolean, varchar) cascade;
 drop type welchs_t_test_results cascade;
-drop function jsonb_transpose(jsonb) cascade;
