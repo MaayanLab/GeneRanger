24a3614f9b59f0000cfa5468ab9b71f0354e71a9
giacomomarino <giacomo_marino@brown.edu>
2023-02-01 19:46:27 -0500

    color/header structure

--- components/header.js
+++ components/header.js
@@ -16,14 +16,13 @@ export default class Header extends React.Component {
                             <h1 style={{fontSize: '40px'}}>GeneRanger</h1>
                         </Link>
                     </div>
+                    <div className={styles.text}><b>GeneRanger is a web-server application that provides access to processed data about the expression of human genes and proteins across human cell types, tissues, and cell lines from several atlases.</b></div>
                     <div className={styles.rightDiv}>
-                        <div className={styles.text}><b>GeneRanger is a web-server application that provides access to processed data about the expression of human genes and proteins across human cell types, tissues, and cell lines from several atlases.</b></div>
-                        <div className={styles.APIandDownloadDiv}>
-                            <div className={styles.textRight}>
-                                <b>Identify Targets on:</b>
-                            </div>
+                        <div className={styles.verticalFlexbox}>
+                            <b style={{fontSize: '18px'}}>Identify Targets with:</b>
                             <Link href={process.env.NEXT_PUBLIC_TARGETRANGERURL} ><a style={{textDecoration: 'none'}}><img src={process.env.NEXT_PUBLIC_ENTRYPOINT + '/images/TargetRangerLogo.png'} alt="Logo" width={50} /><div className={styles.sisterSite}><b>TargetRanger</b></div></a></Link>
                         </div>
+                           
                     </div>
                 </div>
                 <div className={styles.shortTitle}>
