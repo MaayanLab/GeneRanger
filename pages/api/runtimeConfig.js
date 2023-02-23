566d1416d0187cf56213792260d871f27c8b4572
giacomomarino <giacomo_marino@brown.edu>
2023-02-23 11:16:03 -0500

    Update runtimeConfig.js

--- pages/api/runtimeConfig.js
+++ pages/api/runtimeConfig.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
     res.status(200).json({
       NEXT_PUBLIC_ENTRYPOINT: process.env.NEXT_PUBLIC_ENTRYPOINT || '',
       NEXT_PUBLIC_TARGETRANGERURL: process.env.NEXT_PUBLIC_TARGETRANGERURL || '',
+      NEXT_PUBLIC_DOWNLOADS: process.env.NEXT_PUBLIC_DOWNLOADS || '',
     })
   } else {
     res.status(404).end()
