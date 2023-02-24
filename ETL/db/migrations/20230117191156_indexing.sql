5d7cddaae06804efaa4e8a04bea97398e7a72c04
Daniel Clarke <u8sand@gmail.com>
2023-02-24 13:53:37 -0500

    Add unique index to database_agg for concurrent refresh

--- ETL/db/migrations/20230117191156_indexing.sql
+++ ETL/db/migrations/20230117191156_indexing.sql
@@ -1,5 +1,7 @@
 -- migrate:up
+create unique index database_agg_id_idx on database_agg (id);
 create index database_agg_database_idx on database_agg (database);
 
 -- migrate:down
 drop index database_agg_database_idx;
+drop unique index database_agg_id_idx;
