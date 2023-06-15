faf1807fbb51360edbe5098f62ab018999cb674b
giacomomarino <giacomo_marino@brown.edu>
2023-06-15 18:11:20 -0400

    add og meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta property="og:title" content="GeneRanger"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="GeneRanger is a web-server application that provides access to processed data about the expression of human genes and proteins across human cell types, tissues, and cell lines from several atlases."/>
+        <meta property="og:url" content="https://generanger.maayanlab.cloud/"/>
+        <meta property="og:image" content="https://raw.githubusercontent.com/MaayanLab/GeneRanger/main/public/images/logo.png"/>
       </Head>
       <RuntimeConfig>
         <Component {...pageProps} />
